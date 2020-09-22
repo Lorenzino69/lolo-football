5570b5f29712705c816fd9f11dcbdd2c751635d8
lol <lol>
2020-09-23 00:22:31 +0200

    chrono commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -22,20 +22,28 @@ export class HomeComponent implements  OnInit{
     );
   data: any;
   public url: any;
-  private sanitizer: any;
-  controllerSrc: any;
+  private i: number;
+  public team1: Array<any>;
+  public team2: Array<any>;
+
 
   constructor(private breakpointObserver: BreakpointObserver,private leagueService: LeagueService) {
   }
 
   ngOnInit(){
-
+    this.team1 = new Array<any>();
+    this.team2 = new Array<any>();
     window['ga-disable-UA-XXXXX-Y'] = true;
     this.leagueService.getFeed().subscribe(
       res => {
 
         this.data = res.response.g;
         this.url = ('https://www.scorebat.com/embed/g/'+ this.data)
+
+        for(this.i=0; this.i<this.data.length; this.i++){
+          this.team1.push(this.data[this.i].s1.replace('-','').replace(' ','-').toLowerCase());
+          this.team2.push(this.data[this.i].s2.replace('-','').replace(' ','-').toLowerCase());
+        }
       }, () => {
         console.log("erreur d'appel a league service");
       });
